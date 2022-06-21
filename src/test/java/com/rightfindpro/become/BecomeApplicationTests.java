d509a007b611aa0af3ba956e545d1f2fe6617f8d
harrysobit <harrysobit@gmail.com>
2022-06-21 08:40:59 +0545

    initial commit

---
+++ src/test/java/com/rightfindpro/become/BecomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rightfindpro.become;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
